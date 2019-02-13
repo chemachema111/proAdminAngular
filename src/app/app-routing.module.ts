9a0534e1b6c3dd636ea53764dabb56560a8385d3
jose manuel alfonso <jose.manuelchema111@gmail.com>
2019-02-13 17:51:40 +0100

    primer-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,18 @@
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { RegisterComponent } from './register/register.component';
+import { PagesComponent } from './pages/pages.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
+/*import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [];
 
@@ -8,3 +21,25 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+*/
+
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'graficas', component: Graficas1Component},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+
+    ]
+  },
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: NoPageFoundComponent}
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true});
